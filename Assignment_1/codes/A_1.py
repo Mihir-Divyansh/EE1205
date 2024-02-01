dbafd1cfc4fa8710e394a3e153aba1b1c2d0ab27
MD-2005 <jee.emd2005@gmail.com>
2024-02-02 02:09:59 +0530

    Update N+1

--- Assignment_1/codes/A_1.py
+++ Assignment_1/codes/A_1.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 n_1=np.arange(0, 17)
 n_2=np.array([4, 10, 16])
 y1=np.loadtxt("series.txt", delimiter=" ", max_rows=1)
-y2=[y1[i] for i in n_2]
+y2=y1[n_2]
 plt.stem(n_1, y1, markerfmt='.', linefmt='-') 
 plt.stem(n_2, y2, markerfmt='o', linefmt='-') 
 
