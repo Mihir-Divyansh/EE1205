5f2ddb2da8ab6ec4814e99ae621179c2784d95fa
MD-2005 <jee.emd2005@gmail.com>
2024-02-02 22:23:31 +0530

    Update N+2

--- Assignment_1/codes/A_1.py
+++ Assignment_1/codes/A_1.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 n_1=np.arange(0, 17)
-n_2=np.array([4, 10, 16])
+n_2=np.array([3, 9, 15])
 y1=np.loadtxt("series.txt", delimiter=" ", max_rows=1)
 y2=y1[n_2]
 plt.stem(n_1, y1, markerfmt='.', linefmt='-') 
@@ -11,4 +11,4 @@ plt.stem(n_2, y2, markerfmt='o', linefmt='-')
 plt.xlabel('n')
 plt.ylabel('x(n)')
 plt.title('Stem plot of x(n)')
-plt.savefig('A_1.png')
+plt.savefig('../figs/A_1.png')
