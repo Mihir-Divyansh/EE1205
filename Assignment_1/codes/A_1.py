46a33859aa7e8fa953698a0049234956ef92a591
MD-2005 <jee.emd2005@gmail.com>
2024-02-06 10:58:42 +0530

    Update N+3

--- Assignment_1/codes/A_1.py
+++ Assignment_1/codes/A_1.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 n_1=np.arange(0, 17)
 n_2=np.array([3, 9, 15])
-y1=np.loadtxt("series.txt", delimiter=" ", max_rows=1)
+y1=np.loadtxt("series.dat", delimiter=" ", max_rows=1)
 y2=y1[n_2]
 plt.stem(n_1, y1, markerfmt='.', linefmt='-') 
 plt.stem(n_2, y2, markerfmt='o', linefmt='-') 
