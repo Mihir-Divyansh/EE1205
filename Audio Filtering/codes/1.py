6e403066f26233a18551799782f31d622199d893
Parrot User <user@localhost.localdomain>
2024-04-03 07:55:09 +0200

    Audio Filtering

---
+++ Audio Filtering/codes/1.py
@@ -0,0 +1,24 @@
+import soundfile as sf
+import numpy as np
+from scipy import signal
+import scipy
+import librosa
+
+#read .wav file 
+input_signal,f_s = sf.read('My_Voice.wav') 
+
+order=4 #Setting the order of the filter
+
+f_c=4000.0  #cutoff frquency 
+
+Wn=2*f_c/f_s   #digital frequency
+
+# b and a are numerator and denominator polynomials respectively
+b, a = signal.butter(order, Wn, 'low') 
+print(b)
+print(a)
+#filter the input signal with butterworth filter
+output_signal = signal.filtfilt(b, a, input_signal, padlen=1)
+
+#write the output signal into .wav file
+sf.write('My_Voice_With_Reduced_Noise.wav', output_signal, fs) 
